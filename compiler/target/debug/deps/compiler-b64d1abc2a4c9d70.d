6495b37147115977961a1f3d9c4ee3d90da2d933
Rishaad Khan <radishranger123@gmail.com>
2025-02-20 13:25:46 -0500

    Beginning of the compiler

---
+++ compiler/target/debug/deps/compiler-b64d1abc2a4c9d70.d
@@ -0,0 +1,5 @@
+/home/rishaadkhan/Desktop/Projects/qubit-lang/compiler/target/debug/deps/libcompiler-b64d1abc2a4c9d70.rmeta: src/main.rs
+
+/home/rishaadkhan/Desktop/Projects/qubit-lang/compiler/target/debug/deps/compiler-b64d1abc2a4c9d70.d: src/main.rs
+
+src/main.rs:
---
+++ compiler/target/debug/deps/compiler-c48182ed00cc188f.d
@@ -0,0 +1,5 @@
+/home/rishaadkhan/Desktop/Projects/qubit-lang/compiler/target/debug/deps/libcompiler-c48182ed00cc188f.rmeta: src/main.rs
+
+/home/rishaadkhan/Desktop/Projects/qubit-lang/compiler/target/debug/deps/compiler-c48182ed00cc188f.d: src/main.rs
+
+src/main.rs:
