08407e0d4d100fe104bad8f504c22811d1acda0f
Rishaad Khan <radishranger123@gmail.com>
2025-02-20 13:41:30 -0500

    Removed Target folder

--- compiler/target/debug/deps/compiler-b64d1abc2a4c9d70.d
+++
@@ -1,5 +0,0 @@
-/home/rishaadkhan/Desktop/Projects/qubit-lang/compiler/target/debug/deps/libcompiler-b64d1abc2a4c9d70.rmeta: src/main.rs
-
-/home/rishaadkhan/Desktop/Projects/qubit-lang/compiler/target/debug/deps/compiler-b64d1abc2a4c9d70.d: src/main.rs
-
-src/main.rs:
--- compiler/target/debug/deps/compiler-c48182ed00cc188f.d
+++
@@ -1,5 +0,0 @@
-/home/rishaadkhan/Desktop/Projects/qubit-lang/compiler/target/debug/deps/libcompiler-c48182ed00cc188f.rmeta: src/main.rs
-
-/home/rishaadkhan/Desktop/Projects/qubit-lang/compiler/target/debug/deps/compiler-c48182ed00cc188f.d: src/main.rs
-
-src/main.rs:
