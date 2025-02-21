e4599362c12836aee2ec99184b0c3ede39807338
Rishaad Khan <radishranger123@gmail.com>
2025-02-21 14:16:49 -0500

    Just used the old lexer

--- compiler/src/lexer.rs
+++ compiler/src/lexer.rs
@@ -1,73 +1,136 @@
-use std::io::Result;
+use anyhow::{anyhow, Ok, Result};
 
+#[derive(Debug, Clone)]
 pub struct Token {
-    value: String,
-    row: usize
+    pub tok_type: TokenType,
+    pub value: String,
+    pub row: usize,
 }
 
-impl Token {
-    fn new(value: String, row: usize) -> Self {
-        Self { value, row }
-    }    
+#[derive(Debug, Clone, PartialEq, Copy)]
+pub enum TokenType {
+    Word,
+    Str,
+    Number,
 }
 
+#[derive(Debug, Clone)]
 pub struct Lexer;
 
 impl Lexer {
-    // Lexes an entire file 
-    pub fn lex_file(file: &str) -> Result<Vec<Token>> {
-        let mut tokens: Vec<Token> = Vec::new();
-        let program = std::fs::read_to_string(file)?;
-        let lines: Vec<&str> = program.lines().collect();
+    pub fn new() -> Self {
+        Lexer
+    }
+    fn lex_line(&self, file: &str, line: &str, row: usize) -> Result<Vec<Token>> {
+        let split = line.split("//").next().unwrap_or(line);
+        let chars = split.chars().collect::<Vec<char>>();
+        let mut tokens = Vec::<Token>::new();
+        let mut iter: std::iter::Peekable<std::iter::Enumerate<std::slice::Iter<'_, char>>> = chars.iter().enumerate().peekable();
 
-        for (row, &line) in lines.iter().enumerate() {
-            let mut lexed = Self::lex_line(line, row);
-            tokens.append(&mut lexed);
+        while let Some(&(_, &c)) = iter.peek() {
+            match c {
+                '+' | '*' | '/' | '%' | '.' | '&' | '|' | '$' | '@' | '-' | '=' | '(' | ')' => {
+                    let value = c.to_string();
+
+                    tokens.push(Token {
+                        tok_type: TokenType::Word,
+                        value: value,
+                        row,
+                    });
+                    iter.next();
+                }
+                '!' | '<' | '>' => {
+                    let mut expression = c.to_string();
+                    iter.next();
+
+                    if let Some(&(_, '=')) = iter.peek() {
+                        expression.push('=');
+                        iter.next();
+                    }
+
+                    tokens.push(Token {
+                        tok_type: TokenType::Word,
+                        value: expression,
+                        row,
+                    });
+                }
+                '"' => {
+                    iter.next();
+                    let mut value = String::new();
+                    while let Some((_, &next)) = iter.next() {
+                        if next == '"' {
+                            tokens.push(Token {
+                                tok_type: TokenType::Str,
+                                value: value,
+                                row,
+                            });
+                            break;
+                        } else if !next.is_ascii() {
+                            return Err(anyhow!(
+                                "\x1b[31m[ERROR] {file} in line {row}: Non-ascii character encountered\x1b[0m"
+                            ));
+                        } else {
+                            value.push(next);
+                        }
+                    }
+                }
+                _ if c.is_ascii_digit() => {
+                    let mut value = String::new();
+                    while let Some(&(_, &next_c)) = iter.peek() {
+                        if next_c.is_ascii_digit() {
+                            value.push(next_c);
+                            iter.next();
+                        } else {
+                            break;
+                        }
+                    }
+                    tokens.push(Token {
+                        tok_type: TokenType::Number,
+                        value: value,
+                        row,
+                    });
+                }
+                _ if c.is_ascii_alphanumeric() => {
+                    let mut keyword = String::new();
+                    while let Some(&(_, &next_c)) = iter.peek() {
+                        if next_c.is_ascii_alphanumeric() || next_c == '_' {
+                            keyword.push(next_c);
+                            iter.next();
+                        } else {
+                            break;
+                        }
+                    }
+                    tokens.push(Token {
+                        tok_type: TokenType::Word,
+                        value: keyword,
+                        row,
+                    });
+                }
+                ' ' => {
+                    iter.next();
+                }
+                _ => {
+                    return Err(anyhow!(
+                        "\x1b[31m[ERROR] {file} line {row}: Invalid token {c:?}\n[BADCODE] {line}\x1b[0m",
+                        row = row + 1
+                    ));
+                }
+            }
         }
 
         Ok(tokens)
     }
-    
-    // Turns a line into a vector of strings or tokens
-    fn lex_line(line: &str, row: usize) -> Vec<Token> {
-        let split = line.split("//").next().unwrap_or(line);
+
+    pub fn lex_file(&self, file: &str) -> Result<Vec<Token>> {
+        let program = std::fs::read_to_string(file)?;
+        let lines = program.lines().collect::<Vec<&str>>();
         let mut tokens: Vec<Token> = Vec::new();
-        let chars = split.chars().collect::<Vec<char>>();
-        let mut iter = chars.iter().peekable();
 
-        while let Some(&c) = iter.next() {
-            match c {
-                ' ' => (),
-                _ if c.is_alphanumeric() => {
-                    let mut keyword = String::new();
-
-                    while let Some(&&ch) = iter.peek() {
-                        if ch.is_alphanumeric() {
-                            keyword.push(ch);
-                            iter.next();
-                        } else {
-                            tokens.push(Token::new(keyword, row));
-                            break;
-                        }
-                    }
-                }
-                _ if c.is_numeric() => {
-                    let mut number = String::new();
-
-                    while let Some(&&digit) = iter.peek() {
-                        if digit.is_numeric() {
-                            number.push(digit);
-                            iter.next();
-                        } else {
-                            tokens.push(Token::new(number, row));
-                            break;
-                        }
-                    }
-                }
-                _ => eprintln!()
-            }
+        for (row, &line) in lines.iter().enumerate() {
+            let mut lexed = self.lex_line(file, line, row + 1)?;
+            tokens.append(&mut lexed);
         }
 
-        todo!()
+        Ok(tokens)
     }
 }
